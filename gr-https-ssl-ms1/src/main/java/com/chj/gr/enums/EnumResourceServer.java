7518302e4b62ba0697cfd062f0480b8794073074
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-16 12:47:16 +0200

    setup com.chj.gr-common-configs

--- gr-https-ssl-ms1/src/main/java/com/chj/gr/enums/EnumResourceServer.java
+++ gr-https-ssl-ms1/src/main/java/com/chj/gr/enums/EnumResourceServer.java
@@ -4,11 +4,7 @@ package com.chj.gr.enums;
  * ${spring.application.name}-***.yml
  */
 public enum EnumResourceServer {
-	STS_DEFAULT_REGISTRATION("client0"	  ,  "sts-default-registration"),
-	STS_MS1_SERVICE_REGISTRATION("client1",  "sts-ms1-service-registration"),
-	STS_MS2_SERVICE_REGISTRATION("client2",  "sts-ms2-service-registration"),
-	STS_MS3_SERVICE_REGISTRATION("client3",  "sts-ms3-service-registration"),
-	STS_MS4_SERVICE_REGISTRATION("client4",  "sts-ms4-service-registration");
+	STS_DEFAULT_REGISTRATION("client0"	  ,  "sts-default-registration");
 
     private final String key;
     private final String registrationId;
